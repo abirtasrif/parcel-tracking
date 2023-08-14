5a36c7f2d082374ffd830f9890cf60bf245cebc3
abirtasrif <abirtasrif@gmail.com>
2023-08-14 12:17:59 +0600

    chaning image link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="flex flex-col items-center">
-      <img src="../public/image.png" alt="parcel" width="80" height="80" />
+      <img src="../image.png" alt="parcel" width="80" height="80" />
       <h1>Welcome to Parcel Tracking</h1>
       <p>Please select your service provider from above menu </p>
     </div>
