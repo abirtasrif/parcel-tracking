185f107dbb13120c8dc17f75040026e2f51e2708
Abir Tasrif <abirtasrif@gmail.com>
2024-08-12 00:51:44 +0600

    quote design and code structure completed

--- app/components/Finder.jsx
+++ app/components/Finder.jsx
@@ -1,10 +1,9 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Quote from "./Quote";
 
 export default function Finder({ currierCompany }) {
   const [awb, setAwb] = useState("");
-  const [quote, setQuote] = useState({ text: "", author: "" });
-  const [loading, setLoading] = useState(true);
 
   const launchSearch = (awb) => {
     let url;
@@ -32,24 +31,6 @@ export default function Finder({ currierCompany }) {
     event.preventDefault();
     launchSearch(awb);
   };
-
-  const fetchRandomQuote = async () => {
-    try {
-      const response = await fetch("https://quotes-api-self.vercel.app/quote");
-      const quotes = await response.json();
-      // const randomIndex = Math.floor(Math.random() * quotes.length);
-      // setQuote(quotes[randomIndex]);
-      setQuote(quotes);
-      setLoading(false);
-    } catch (error) {
-      console.error("Failed to fetch quote", error);
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchRandomQuote();
-  }, []);
 
   return (
     <div className="bg-white py-16 sm:py-24 lg:py-32 flex flex-col">
@@ -88,17 +69,7 @@ export default function Finder({ currierCompany }) {
           You may change provider from top left menu
         </p>
       </div>
-
-      {loading ? (
-        <div className="quote-container flex flex-col justify-center text-center">
-          Loading...
-        </div>
-      ) : (
-        <div className="quote-container flex flex-col justify-center text-center">
-          <p className="q-text">{quote.quote}</p>
-          <p className="q-author">{quote.author || "Unknown"}</p>
-        </div>
-      )}
+      <Quote />
     </div>
   );
 }
